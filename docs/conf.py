7f5386109ccfa4e8609a05e752f5570894e4c7b9
RipamontiD <ripabest2007@gmail.com>
2025-12-11 17:50:20 +0100

    configurazione CORRETTA di sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -13,10 +13,13 @@ author = 'Ripamonti, Colombi'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+sys.path.insert(0, os.path.abspath('..'))
+
 extensions = [
     'sphinx.ext.autodoc',       # Legge le docstring
     'sphinx.ext.napoleon',      # Supporta stile Google/NumPy
-    'sphinx.ext.viewcode',      # Aggiunge link al codice sorgente]
+    'sphinx.ext.viewcode',      # Aggiunge link al codice sorgente
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -26,5 +29,5 @@ language = 'it'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
